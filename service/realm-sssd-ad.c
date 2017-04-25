mikhailnov/realmd

commit 402cbab6e8267fcd959bcfa84a47f4871b59944d

service: Add nss and pam sssd.conf services after joining
After adding a domain to sssd.conf add the nss and pam services
to the [sssd] block.

https://bugs.freedesktop.org/show_bug.cgi?id=98479

service/realm-sssd-ad.c
@@ -160,6 +160,7 @@ configure_sssd_for_domain (RealmIniConfig *config,
                            gboolean use_adcli,
                            GError **error)
 {
+	const gchar *services[] = { "nss", "pam", NULL };
 	GString *realmd_tags;
 	const gchar *access_provider;
 	const gchar *shell;
@@ -205,6 +206,8 @@ configure_sssd_for_domain (RealmIniConfig *config,
 	                                    "ad_server", disco->explicit_server,
 	                                    "ldap_sasl_authid", authid,
 	                                    NULL);
+
+	realm_ini_config_set_list_diff (config, "sssd", "services", ", ", services, NULL);
 
 	g_free (authid);
 	g_string_free (realmd_tags, TRUE);

service/realm-sssd-config.c
@@ -154,8 +154,6 @@ realm_sssd_config_add_domain (RealmIniConfig *config,
 	g_strfreev (already);
 
 	/* Setup a default sssd section */
-	if (!realm_ini_config_have (config, "section", "services"))
-		realm_ini_config_set (config, "sssd", "services", "nss, pam", NULL);
 	if (!realm_ini_config_have (config, "sssd", "config_file_version"))
 		realm_ini_config_set (config, "sssd", "config_file_version", "2", NULL);
 

service/realm-sssd-ipa.c
@@ -156,6 +156,7 @@ on_ipa_client_do_restart (GObject *source,
                           GAsyncResult *result,
                           gpointer user_data)
 {
+	const gchar *services[] = { "nss", "pam", NULL };
 	GTask *task = G_TASK (user_data);
 	EnrollClosure *enroll = g_task_get_task_data (task);
 	RealmSssd *sssd = g_task_get_source_object (task);
@@ -206,6 +207,8 @@ on_ipa_client_do_restart (GObject *source,
 		                                 "fallback_homedir", home,
 		                                 "realmd_tags", realmd_tags,
 		                                 NULL);
+
+		realm_ini_config_set_list_diff (config, "sssd", "services", ", ", services, NULL);
 
 		g_free (home);
 	}

tests/test-sssd-config.c
@@ -90,7 +90,7 @@ test_add_domain (Test *test,
                  gconstpointer unused)
 {
 	const gchar *data = "[domain/one]\nval=1\n[sssd]\ndomains=one";
-	const gchar *check = "[domain/one]\nval=1\n[sssd]\ndomains = one, two\nconfig_file_version = 2\nservices = nss, pam\n\n[domain/two]\ndos = 2\n";
+	const gchar *check = "[domain/one]\nval=1\n[sssd]\ndomains = one, two\nconfig_file_version = 2\n\n[domain/two]\ndos = 2\n";
 	GError *error = NULL;
 	gchar *output;
 	gboolean ret;
@@ -140,7 +140,7 @@ static void
 test_add_domain_only (Test *test,
                       gconstpointer unused)
 {
-	const gchar *check = "\n[sssd]\ndomains = two\nconfig_file_version = 2\nservices = nss, pam\n\n[domain/two]\ndos = 2\n";
+	const gchar *check = "\n[sssd]\ndomains = two\nconfig_file_version = 2\n\n[domain/two]\ndos = 2\n";
 	GError *error = NULL;
 	gchar *output;
 	gboolean ret;

commit 9d5b6f5c88df582fb94edcf5cc05a8cfaa63cf6a

service: Add "pam" and "nss" services in realm_sssd_config_add_domain()
realm_sssd_config_add_domain() must setup the services line in sssd.conf
otherwise SSSD won't be able to start any of its services.

It's a regression caused by 402cbab which leaves SSSD with no services
line when joining to an ad client doing "realm join ad.example".

https://bugs.freedesktop.org/show_bug.cgi?id=98479

Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>

service/realm-sssd-ad.c
@@ -207,7 +207,8 @@ configure_sssd_for_domain (RealmIniConfig *config,
 	                                    "ldap_sasl_authid", authid,
 	                                    NULL);
 
-	realm_ini_config_set_list_diff (config, "sssd", "services", ", ", services, NULL);
+	if (ret)
+		ret = realm_ini_config_change_list (config, "sssd", "services", ", ", services, NULL, error);
 
 	g_free (authid);
 	g_string_free (realmd_tags, TRUE);

service/realm-sssd-config.c
@@ -130,6 +130,7 @@ realm_sssd_config_add_domain (RealmIniConfig *config,
 	gchar **already;
 	gboolean ret;
 	gchar *section;
+	const gchar *services[] = { "nss", "pam", NULL };
 	va_list va;
 	gint i;
 
@@ -154,6 +155,7 @@ realm_sssd_config_add_domain (RealmIniConfig *config,
 	g_strfreev (already);
 
 	/* Setup a default sssd section */
+	realm_ini_config_set_list_diff (config, "sssd", "services", ", ", services, NULL);
 	if (!realm_ini_config_have (config, "sssd", "config_file_version"))
 		realm_ini_config_set (config, "sssd", "config_file_version", "2", NULL);
 

service/realm-sssd-ipa.c
@@ -208,7 +208,8 @@ on_ipa_client_do_restart (GObject *source,
 		                                 "realmd_tags", realmd_tags,
 		                                 NULL);
 
-		realm_ini_config_set_list_diff (config, "sssd", "services", ", ", services, NULL);
+		if (error == NULL)
+			realm_ini_config_change_list (config, "sssd", "services", ", ", services, NULL, &error);
 
 		g_free (home);
 	}

tests/test-sssd-config.c
@@ -90,7 +90,7 @@ test_add_domain (Test *test,
                  gconstpointer unused)
 {
 	const gchar *data = "[domain/one]\nval=1\n[sssd]\ndomains=one";
-	const gchar *check = "[domain/one]\nval=1\n[sssd]\ndomains = one, two\nconfig_file_version = 2\n\n[domain/two]\ndos = 2\n";
+	const gchar *check = "[domain/one]\nval=1\n[sssd]\ndomains = one, two\nconfig_file_version = 2\nservices = nss, pam\n\n[domain/two]\ndos = 2\n";
 	GError *error = NULL;
 	gchar *output;
 	gboolean ret;
@@ -140,7 +140,7 @@ static void
 test_add_domain_only (Test *test,
                       gconstpointer unused)
 {
-	const gchar *check = "\n[sssd]\ndomains = two\nconfig_file_version = 2\n\n[domain/two]\ndos = 2\n";
+	const gchar *check = "\n[sssd]\ndomains = two\nconfig_file_version = 2\nservices = nss, pam\n\n[domain/two]\ndos = 2\n";
 	GError *error = NULL;
 	gchar *output;
 	gboolean ret;